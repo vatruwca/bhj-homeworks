a9ea9799b0f57dac1bae2c9a07f0a572a9f69d41
vatruwca <vatruwca@gmail.com>
2022-04-18 10:04:14 +0300

    file changed

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,10 +1,8 @@
 
-
-const clockInterval = setInterval(function(){
-  let timer = document.getElementById("timer");
-  const endTime = new Date(2022, 3, 16, 14, 9).getTime();
-  let now = new Date().getTime(); 
-  let t = endTime - now;
+let timer = document.getElementById("timer");
+const endTime = new Date(Date.now() + 5400999);
+function countdownTimer() {
+  const t = endTime - new Date();
     if (t >= 0) {
        let hours = Math.floor(t / (1000 * 60 * 60));
        let minutes = Math.floor(t % (1000 * 60 * 60) / (1000 * 60));
@@ -14,8 +12,11 @@ const clockInterval = setInterval(function(){
        seconds = seconds < 10 ? `0` + seconds : seconds;
        timer.textContent = hours + ':' + minutes + ':' + seconds;
     } else {
-         clearInterval(clockInterval);
+        clearInterval(clockInterval);
         location = 'https://docs.google.com/presentation/d/12OQKEFSTBA1Z3IXtuJTbHSUCyw87cdj1UuIkedm1ux4/edit?usp=sharing';  
         alert("«Вы победили в конкурсе!»");
       }
-}, 1000)
+};
+
+countdownTimer();
+const clockInterval = setInterval(countdownTimer, 1000);
